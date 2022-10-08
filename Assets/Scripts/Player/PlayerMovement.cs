33c815c96f32b1e34e7bf667926962bacc8fba19
Minh Tú Lê <leminhtu74@gmail.com>
2022-10-08 16:24:37 +0700

    merge main into create_terrain

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private BoxCollider2D boxCollider;
-    private Animator animator;
-    private RaycastHit2D hit;
+    
+    [SerializeField] float m_Speed = 10f;
 
-    [SerializeField] float speed = 10f;
-
-    private Vector3 moveDelta;
+    private Animator m_Animator;
+    private RaycastHit2D m_Hit;
+    private BoxCollider2D m_BoxCollider;
+    private Vector3 m_moveDelta;
 
     private void Start()
     {
-        // boxCollider = GetComponent<BoxCollider2D>();    
-        animator = GetComponent<Animator>();  
+        m_BoxCollider = GetComponent<BoxCollider2D>();    
+        m_Animator = GetComponent<Animator>();  
     }
 
     private void FixedUpdate() 
@@ -23,44 +23,38 @@ public class PlayerMovement : MonoBehaviour
         float dirX = Input.GetAxisRaw("Horizontal");
         float dirY = Input.GetAxisRaw("Vertical");
 
-        moveDelta = new Vector3(dirX, dirY, 0);
+        m_moveDelta = new Vector3(dirX, dirY, 0);
 
-        if (moveDelta != Vector3.zero)
-            animator.SetBool("running", true);
+        if (m_moveDelta != Vector3.zero)
+            m_Animator.SetBool("running", true);
         else
-            animator.SetBool("running", false);
+            m_Animator.SetBool("running", false);
 
-        if (moveDelta.x > 0)
+        if (m_moveDelta.x > 0)
             transform.localScale = Vector3.one;
-        else if (moveDelta.x < 0)
+        else if (m_moveDelta.x < 0)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        float step = Time.deltaTime * speed;
-        float deltaY = moveDelta.y * step;
-        float deltaX = moveDelta.x * step;
+        float step = Time.deltaTime * m_Speed;
+        float deltaY = m_moveDelta.y * step;
+        float deltaX = m_moveDelta.x * step;
 
-        hit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.size, 0, 
-                                new Vector2(0, moveDelta.y),
+        m_Hit = Physics2D.BoxCast(m_BoxCollider.bounds.center, m_BoxCollider.size, 0, 
+                                new Vector2(0, m_moveDelta.y),
                                 Mathf.Abs(deltaY),
                                 LayerMask.GetMask("Blocking"));
-        if (hit.collider == null)
+        if (m_Hit.collider == null)
         {
             transform.Translate(0, deltaY, 0);
         }
 
-        hit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.size, 0, 
-                                new Vector2(moveDelta.x, 0),
+        m_Hit = Physics2D.BoxCast(m_BoxCollider.bounds.center, m_BoxCollider.size, 0, 
+                                new Vector2(m_moveDelta.x, 0),
                                 Mathf.Abs(deltaX),
                                 LayerMask.GetMask("Blocking"));
-        if (hit.collider == null)
+        if (m_Hit.collider == null)
         {
             transform.Translate(deltaX, 0, 0);
         }
-    }
-
-    private void OnDrawGizmos() 
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(boxCollider.bounds.center, boxCollider.size); 
     }
 }
