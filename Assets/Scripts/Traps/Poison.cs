c565fa0702440c00928890738b6a4915e8c7ef69
hoang1645 <hoang.208.1974@gmail.com>
2022-10-23 23:57:04 +0700

    wtf

---
+++ Assets/Scripts/Traps/Poison.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Traps
+{
+    public class Poison : Trap
+    {
+        protected Animator dropletAnimator;
+        protected new void Start()
+        {
+            animator = GetComponent<Animator>();
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            script = new Dictionary<string, MonoBehaviour>();
+            foreach (MonoBehaviour s in GetComponents<MonoBehaviour>())
+            {
+                script[s.name] = s;
+            }
+            colliders = GetComponents<Collider2D>();
+
+        }
+    }
+}
