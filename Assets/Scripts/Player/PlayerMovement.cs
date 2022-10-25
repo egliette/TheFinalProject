c8eaf8372d97c7409bf7c67ceaccf672153dd59c
egliette <77763935+egliette@users.noreply.github.com>
2022-10-25 22:33:18 +0700

    Player weapon (#5)
    * add bullet prefab
    
    * add weapon sprites
    
    * update healthbar and player prefab
    
    * create bullet animation
    
    * flip character sprite when moving mouse
    
    * add shooting scripts
    
    * add weapon to player

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,37 +10,36 @@ public class PlayerMovement : MonoBehaviour
     private Animator m_Animator;
     private RaycastHit2D m_Hit;
     private BoxCollider2D m_BoxCollider;
-    private Vector3 m_moveDelta;
+    private SpriteRenderer m_Sprite;
+    private Vector3 m_MoveDelta;
 
     private void Start()
     {
         m_BoxCollider = GetComponent<BoxCollider2D>();    
         m_Animator = GetComponent<Animator>();  
+        m_Sprite = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate() 
     {
+        FaceMouse();
+
         float dirX = Input.GetAxisRaw("Horizontal");
         float dirY = Input.GetAxisRaw("Vertical");
 
-        m_moveDelta = new Vector3(dirX, dirY, 0);
+        m_MoveDelta = new Vector3(dirX, dirY, 0);
 
-        if (m_moveDelta != Vector3.zero)
+        if (m_MoveDelta != Vector3.zero)
             m_Animator.SetBool("running", true);
         else
             m_Animator.SetBool("running", false);
 
-        if (m_moveDelta.x > 0)
-            transform.localScale = Vector3.one;
-        else if (m_moveDelta.x < 0)
-            transform.localScale = new Vector3(-1, 1, 1);
-
         float step = Time.deltaTime * m_Speed;
-        float deltaY = m_moveDelta.y * step;
-        float deltaX = m_moveDelta.x * step;
+        float deltaY = m_MoveDelta.y * step;
+        float deltaX = m_MoveDelta.x * step;
 
         m_Hit = Physics2D.BoxCast(m_BoxCollider.bounds.center, m_BoxCollider.size, 0, 
-                                new Vector2(0, m_moveDelta.y),
+                                new Vector2(0, m_MoveDelta.y),
                                 Mathf.Abs(deltaY),
                                 LayerMask.GetMask("Blocking"));
         if (m_Hit.collider == null)
@@ -49,12 +48,25 @@ public class PlayerMovement : MonoBehaviour
         }
 
         m_Hit = Physics2D.BoxCast(m_BoxCollider.bounds.center, m_BoxCollider.size, 0, 
-                                new Vector2(m_moveDelta.x, 0),
+                                new Vector2(m_MoveDelta.x, 0),
                                 Mathf.Abs(deltaX),
                                 LayerMask.GetMask("Blocking"));
         if (m_Hit.collider == null)
         {
             transform.Translate(deltaX, 0, 0);
         }
+    }
+
+    private void FaceMouse()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 screenPoint = Camera.main.ScreenToWorldPoint(transform.position);
+        Vector2 direction = mousePos - (Vector2)transform.position;
+        
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        if (rotZ < 90 && rotZ > -90)
+            m_Sprite.flipX = false;
+        else
+            m_Sprite.flipX = true;
     }
 }
