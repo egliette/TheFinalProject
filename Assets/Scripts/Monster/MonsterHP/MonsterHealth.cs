f1937fd6f4b2ac074127f16782a04c02f919766a
Minh Tú Lê <leminhtu74@gmail.com>
2022-10-28 20:10:55 +0700

    create hurt animation

--- Assets/Scripts/Monster/MonsterHP/MonsterHealth.cs
+++ Assets/Scripts/Monster/MonsterHP/MonsterHealth.cs
@@ -6,20 +6,34 @@ using UnityEngine;
 public class MonsterHealth : MonoBehaviour
 {
     [SerializeField] private Monster m_Monster;
+    [SerializeField] private SpriteRenderer m_Sprite;
+
+    private Color originColor;
     private float m_StartHealth;
     private float m_CurrentHealth;
     private bool m_Dead;
+    private float m_HurtDelayTime = 0.2f;
+
 
     private void OnEnable()
     {
         m_Dead = false;
+        originColor = m_Sprite.color;
     }
-
 
     private void OnDeath()
     {
         m_Dead = true;
         m_Monster.OnDeadUI();
+    }
+
+    private IEnumerator Hurt()
+    {
+        m_Sprite.color = Color.red;
+
+        yield return new WaitForSeconds(m_HurtDelayTime);
+
+        m_Sprite.color = originColor;
     }
 
 
@@ -34,14 +48,16 @@ public class MonsterHealth : MonoBehaviour
         m_CurrentHealth = m_StartHealth;
     }
 
-
     public void TakeDamage(float amount)
     {
-        Debug.Log("TAKEDAMAGE");
         m_CurrentHealth -= amount;
         if (m_CurrentHealth <= 0 && !m_Dead)
         {
             OnDeath();
+        }
+        else
+        {
+            StartCoroutine(Hurt());
         }
     }
 
